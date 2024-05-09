cf9f34eeb8a24102c1305407888fb08cfa59c94e
Franklin Fomo <fomo_tchamda.franklin_bernadin@courrier.uqam.ca>
2024-05-09 19:11:18 -0400

    Commit init

---
+++ src/bonjournom/java/BonjourNom.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjournom.java;
+
+/**
+ *
+ * @author fomof
+ */
+import aleatoire.Person;
+public class BonjourNom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World !");
+    }
+}
